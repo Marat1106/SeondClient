3f701589e765a17f48c17186e0c052489a71902e
Marat Mukiyat <m.mukiyat@astanait.edu.kz>
2021-01-16 19:20:48 +0600

    second person

---
+++ SecondClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using Chat_Project;
+using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace SecondClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Enter your name:");
+            var username = Console.ReadLine();
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new ChatService.ChatServiceClient(channel);
+
+            using (var chat = client.Join())
+            {
+                _ = Task.Run(async () =>
+                {
+                    while (await chat.ResponseStream.MoveNext())
+                    {
+                        var response = chat.ResponseStream.Current;
+                        Console.WriteLine($"{response.User}:{response.Text}");
+                    }
+                });
+
+
+
+
+                await chat.RequestStream.WriteAsync(new Message { User = username, Text = $"{username} has joined the chat" });
+                string line;
+                while ((line = Console.ReadLine()) != null)
+                {
+                    if (line.ToUpper() == "EXIT")
+                    {
+                        break;
+                    }
+                    await chat.RequestStream.WriteAsync(new Message { User = username, Text = line });
+                }
+                await chat.RequestStream.CompleteAsync();
+            }
+            Console.WriteLine("He(Her)is leaving");
+            await channel.ShutdownAsync();
+        }
+    }
+}
